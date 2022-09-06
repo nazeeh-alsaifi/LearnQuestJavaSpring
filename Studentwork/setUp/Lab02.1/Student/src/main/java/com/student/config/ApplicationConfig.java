571f8c1cd02785351920f14a89957614fbf6fd43
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-09-06 09:21:21 +0300

    end of lab 7.1

--- Studentwork/setUp/Lab02.1/Student/src/main/java/com/student/config/ApplicationConfig.java
+++ Studentwork/setUp/Lab02.1/Student/src/main/java/com/student/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.student.config;
 import javax.inject.Inject;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,10 +13,14 @@ import com.student.dao.StudentDao;
 import com.student.dao.StudentDaoImpl;
 import com.student.service.StudentService;
 import com.student.service.StudentServiceImpl;
+import com.student.service.StudentServiceProxy;
 
 @Configuration
-@PropertySource(value="classpath:application.properties")
+@PropertySource(value = "classpath:application.properties")
 public class ApplicationConfig {
+
+    @Value("${max}")
+    private int numberOfStudents;
 
     @Bean("studentDao")
     StudentDao getStudentDao() {
@@ -23,16 +28,26 @@ public class ApplicationConfig {
     }
 
     @Bean("studentService")
-    StudentService getStudentService(@Value("${max}") int numberOfStudents) {
+    @ConditionalOnProperty(value = "env", havingValue = "prod", matchIfMissing = true)
+    StudentService getStudentService() {
         StudentServiceImpl service = new StudentServiceImpl();
         service.setStudentDao(getStudentDao());
-        service.setNumberOfStudents(numberOfStudents);
+        service.setNumberOfStudents(this.numberOfStudents);
         return service;
     }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean(name = "studentService")
+    @ConditionalOnProperty(value = "env", havingValue = "test", matchIfMissing = true)
+    StudentService serviceTest() {
+        StudentServiceImpl service = new StudentServiceImpl();
+        service.setStudentDao(getStudentDao());
+        service.setNumberOfStudents(this.numberOfStudents);
+        return new StudentServiceProxy(service);
     }
 
 }
