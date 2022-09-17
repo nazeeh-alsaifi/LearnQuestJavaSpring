483282e3f9ac5a040896c3214e9ed6b373bd5dd9
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-09-17 17:36:14 +0300

    end of lab3

--- spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/StudentApplication.java
+++ spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/StudentApplication.java
@@ -17,8 +17,8 @@ public class StudentApplication {
 	}
 	
 	@GetMapping
-	String home() {
-		return "Hello World";
+	RedirectView home() {
+		return new RedirectView("student/msg");
 	}
 
 }
--- spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/controller/StudentController.java
+++ spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/controller/StudentController.java
@@ -1,24 +1,37 @@
 package com.student.controller;
 
- 
+import java.util.Collection;
+
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
-//TODO
-//TODO
+
+import com.student.core.Student;
+import com.student.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
-	
-	 
+
+	@Value("${message}")
 	private String message;
- 
-	
- 
+
+	@Inject
+	private StudentService service;
+
+	@GetMapping(path = "msg")
 	public String getMessage() {
 		return message;
 	}
-	
- 
+
+	//we can get rid of the produces argument because its the default value
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Student> getAll(){
+		return service.getAllStudents();
+	}
 
 }
