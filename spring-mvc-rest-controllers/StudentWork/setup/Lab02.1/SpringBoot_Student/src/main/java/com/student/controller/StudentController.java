63cb2586e4b37b4b9718c8d7dadef6b899ca48fb
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-09-29 09:06:54 +0300

    lab5.1 end

--- spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/controller/StudentController.java
+++ spring-mvc-rest-controllers/StudentWork/setup/Lab02.1/SpringBoot_Student/src/main/java/com/student/controller/StudentController.java
@@ -1,13 +1,17 @@
 package com.student.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.StudentProperties;
@@ -28,15 +32,24 @@ public class StudentController {
 	private StudentProperties properties;
 
 	@GetMapping(path = "msg")
-	public String getMessage() {
+	public String getMessage(@RequestHeader("user-agent") String agent) {
 		// return message;
-		return properties.getGreetings();
+		return properties.getGreetings() + " " + agent;
 	}
 
-	//we can get rid of the produces argument because its the default value
+	// we can get rid of the produces argument because its the default value
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public Collection<Student> getAll(){
+	public Collection<Student> getAll() {
 		return service.getAllStudents();
 	}
 
+	@GetMapping("/{id}")
+	public Student getStudent(@PathVariable("id") long id) {
+		return service.get(id);
+	}
+
+	@GetMapping("/single")
+	public Student getSingleStudent(@RequestParam("id") Optional<Long> optional) {
+		return service.get(optional.orElse(1l));
+	}
 }
