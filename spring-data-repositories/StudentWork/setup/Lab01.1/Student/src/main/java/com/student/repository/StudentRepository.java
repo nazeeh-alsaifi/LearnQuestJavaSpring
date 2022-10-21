7ac7393622a02b8a44368aa059ed45ac7069187f
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-10-21 14:23:53 +0300

    end lab 2.3

--- spring-data-repositories/StudentWork/setup/Lab01.1/Student/src/main/java/com/student/repository/StudentRepository.java
+++ spring-data-repositories/StudentWork/setup/Lab01.1/Student/src/main/java/com/student/repository/StudentRepository.java
@@ -5,9 +5,10 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.student.core.Student;
-
+@RepositoryRestResource(collectionResourceRel = "student", path = "enrollments")
 public interface StudentRepository extends JpaRepository<Student,Long> {
     Collection<Student> findByDept(String department);
 
