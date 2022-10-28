f255bf2fe15d93ae3c5ce0b9f6fd253f94c4c6a1
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-10-28 11:47:07 +0200

    end lab 3.2

--- spring-data-repositories/StudentWork/setup/Lab01.1/Student/src/main/java/com/student/aspect/StudentAspect.java
+++ spring-data-repositories/StudentWork/setup/Lab01.1/Student/src/main/java/com/student/aspect/StudentAspect.java
@@ -3,12 +3,16 @@ package com.student.aspect;
 import javax.inject.Named;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.student.core.Student;
 
 @Aspect
 @Named
@@ -27,6 +31,14 @@ public class StudentAspect {
     @After("log()")
     public void afterAdvice(JoinPoint jp){
         LOG.info("Invoked Method After -> " + jp.getSignature().getName());
+    }
+
+    @Around("log() && args(id)")
+    public Object around(ProceedingJoinPoint pjp, long id) throws Throwable {
+        LOG.info("Around before -> " + pjp.getSignature().getName() + " with id " + id);
+        Student student = (Student) pjp.proceed();
+        LOG.info("Around after -> " + student.getFirstName() + " " + student.getSurname());
+        return student;
     }
 
 }
