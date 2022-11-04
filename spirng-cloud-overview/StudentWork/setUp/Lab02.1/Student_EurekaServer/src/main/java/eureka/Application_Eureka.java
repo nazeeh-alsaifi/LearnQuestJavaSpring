d2c96a9edcfd9d320efa43181d2d409269a7873e
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2022-11-04 14:54:33 +0300

    end of lab 2.1

--- spirng-cloud-overview/StudentWork/setUp/Lab02.1/Student_EurekaServer/src/main/java/eureka/Application_Eureka.java
+++ spirng-cloud-overview/StudentWork/setUp/Lab02.1/Student_EurekaServer/src/main/java/eureka/Application_Eureka.java
@@ -3,13 +3,13 @@ package eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
- 
+
+@EnableEurekaServer
 @SpringBootApplication
-//<TODO
 public class Application_Eureka {
 
     public static void main(String[] args) {
-    	SpringApplication.run(Application_Eureka.class, args);
+        SpringApplication.run(Application_Eureka.class, args);
     }
 
 }
